69cf505d25303aceff2039ecd3540ecf7ab196cf
gzf <gaozefeng@hust.edu.cn>
2017-01-05 19:48:37 +0800

    delete useless sql script

--- users.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `users` (`id`, `ipaddress`, `currentlogin`, `online`, `connectstatus`, `connectwith`, `operationcount`) VALUES (80, '115.156.164.37', '2017-1-4 20:42:05', 0, 0, '无', 0);
-INSERT INTO `users` (`id`, `ipaddress`, `currentlogin`, `online`, `connectstatus`, `connectwith`, `operationcount`) VALUES (87, '115.156.164.123', '2017-1-4 21:04:16', 0, 0, '无', 0);
